0beecd0ecbf1d6d8a8ed7adb7c95356d3d9dbe72
Efehan Kalender <efehankalender5206@gmail.com>
2026-07-26 16:26:55 +0300

    Configure production API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+export default axios.create({
+  baseURL: "https://link-shortener-api-z6yx.onrender.com/api",
 });
-
-export default api;
